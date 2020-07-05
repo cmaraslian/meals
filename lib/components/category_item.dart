aca219374558a181fbb4da13eb3575e89e698c2b
unknown <cmaraslian@hotmail.com>
2020-07-05 20:41:55 -0300

    Novas fontes adicionadas

---
+++ lib/components/category_item.dart
@@ -0,0 +1,28 @@
+import 'package:Meals/models/category.dart';
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  
+  final Category category;
+
+  CategoryItem(this.category);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      child: Text(category.title),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        gradient: LinearGradient(
+          colors: [
+            category.color.withOpacity(0.5),
+            category.color,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight
+          )
+      ),
+    );
+  }
+}
--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,16 +1,26 @@
-import 'package:flutter/cupertino.dart';
+import 'package:Meals/components/category_item.dart';
+import 'package:Meals/data/dummy_data.dart';
+import 'package:flutter/material.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vamos Cozinhar?'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25) ,
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
         ),
-      
+        children: DUMMY_CATEGORIES.map((cat) {
+          return CategoryItem(cat);
+        }).toList(),
+      ),
     );
   }
 }
