f9d7607aaf65ac83f3c328e5c62e6f5990e65c0f
Carlos A. Maraslian <cmaraslian@hotmail.com>
2020-07-06 01:17:13 -0300

    Configuração de tema e estilo

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -2,27 +2,21 @@ import 'package:Meals/models/category.dart';
 import 'package:flutter/material.dart';
 
 class CategoryItem extends StatelessWidget {
-  
   final Category category;
 
   CategoryItem(this.category);
-  
+
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(15),
-      child: Text(category.title),
+      child: Text(category.title, style: Theme.of(context).textTheme.headline6),
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        gradient: LinearGradient(
-          colors: [
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(colors: [
             category.color.withOpacity(0.5),
             category.color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight
-          )
-      ),
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
     );
   }
 }
