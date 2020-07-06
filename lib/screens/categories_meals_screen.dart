d829d19d18fc8664f99d5d4642f1172f9254c246
Carlos A. Maraslian <cmaraslian@hotmail.com>
2020-07-06 02:19:59 -0300

    Início criação telas de navegação

---
+++ lib/screens/categories_meals_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import '../models/category.dart';
+
+class CategoriesMealsScreen extends StatelessWidget {
+  final Category category;
+
+  const CategoriesMealsScreen(this.category);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(category.title),
+      ),
+      body: Center(
+        child: Text('Receita por Categoria ${category.id}'),
+      ),
+    );
+  }
+}
